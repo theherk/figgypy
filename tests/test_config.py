05e06fd154886c9578f93d77de63b78a7cadb8b9
Herkermer Sherwood <theherk@gmail.com>
2015-10-25 16:59:26 -0700

    Point to the proper files

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,10 +6,10 @@ import os
 
 class TestConfig(unittest.TestCase):
     def test_config_load(self):
-        os.environ['FIGGY_GPG_HOME']='test-keys'
-        c = figgypy.config.Config('test-config.json')
-        self.assertEquals(c.db, 'db.heck.ya')
-        self.assertEquals(c.secrets['hush_hush'], 'i-u-i')
+        os.environ['FIGGY_GPG_HOME']='tests/resources/test-keys'
+        c = figgypy.config.Config('tests/resources/test-config.json')
+        self.assertEqual(c.db, 'db.heck.ya')
+        self.assertEqual(c.secrets['hush_hush'], 'i-u-i')
 
 
 if __name__ == '__main__':
