fd62bc4ed4111fa274f753ea97452b22e642e1e7
Herkermer Sherwood <theherk@gmail.com>
2015-10-25 18:48:29 -0700

    Test with yaml because the armor is easier to deal with

--- tests/test_config.py
+++ tests/test_config.py
@@ -7,7 +7,7 @@ import os
 class TestConfig(unittest.TestCase):
     def test_config_load(self):
         os.environ['FIGGY_GPG_HOME']='tests/resources/test-keys'
-        c = figgypy.config.Config('tests/resources/test-config.json')
+        c = figgypy.config.Config('tests/resources/test-config.yaml')
         self.assertEqual(c.db, 'db.heck.ya')
         self.assertEqual(c.secrets['hush_hush'], 'i-u-i')
 
