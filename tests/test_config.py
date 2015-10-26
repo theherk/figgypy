c96b318166c19b644071386380e3b9b6b32deae6
Herkermer Sherwood <theherk@gmail.com>
2015-10-25 18:53:28 -0700

    Modify to test with nesting

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,8 +8,8 @@ class TestConfig(unittest.TestCase):
     def test_config_load(self):
         os.environ['FIGGY_GPG_HOME']='tests/resources/test-keys'
         c = figgypy.config.Config('tests/resources/test-config.yaml')
-        self.assertEqual(c.db, 'db.heck.ya')
-        self.assertEqual(c.secrets['hush_hush'], 'i-u-i')
+        self.assertEqual(c.db['host'], 'db.heck.ya')
+        self.assertEqual(c.db['pass'], 'test password')
 
 
 if __name__ == '__main__':
