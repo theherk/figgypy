9c792291cdcae39d312a0e9218a843cfd951e3e5
Herkermer Sherwood <theherk@gmail.com>
2015-10-27 14:06:36 -0700

    Add test for no python-gnupgp

--- tests/test_config.py
+++ tests/test_config.py
@@ -11,6 +11,31 @@ class TestConfig(unittest.TestCase):
         self.assertEqual(c.db['host'], 'db.heck.ya')
         self.assertEqual(c.db['pass'], 'test password')
 
+    def test_config_load_without_gpg(self):
+        figgypy.config.gpg_loaded = False
+        c = figgypy.config.Config('tests/resources/test-config.yaml')
+        encrypted_password = (
+            '-----BEGIN PGP MESSAGE-----\n'
+            'Version: GnuPG v2\n'
+            '\n'
+            'hQIMAzf92ZrOUZL3ARAAgWexav8+pc2lnqISEuQafFZrqYI0pU3xCuMXnFZp+hpU\n'
+            'gb0LsaExZ136p4ATIinFHuaLt94hFx7gULgqoSigt/2fubnUCsOGedq122xYZdtV\n'
+            'Ep/24WPVQPcMVIP9pDTJTk82A41BQsOrVYorAGjjB13zFizizYHApNTcWKr4/gfR\n'
+            'jmCqAX5qusXB84fXBecCJ886uEQI2v7+Vxnk+fQMqNt3ybd/uLuBLShMSygr6uLX\n'
+            'zktyeZvP2QqPSWe0OpttdcvD792/SI/CTznsjbMe0wr1L81csEQcj++4o5wJop3Y\n'
+            'mbQvG/FxeDdRi2aCxh7JK2xdCsrQzXKTNG2QZMwWqatB5Lb6lJ1mNiJQGX2YK+nI\n'
+            'lbjy5Cp2lHlNxa9QfB+KglueMnH9gDku5YqBDos6rCEuqK/aTDdMx0V7YGYTamZ3\n'
+            '3Za+OGi+hl/+4WX2gm+bOM2WWrIysiu9k1HMI1/onui/3hr1nClR8rGb4a5qDlpg\n'
+            'yRrt7LuLRU4vGXpYm05dXlUeI3uT04ur/DwLo32ujnPo3dc8LFegX8N8p1LLS9vq\n'
+            'vvrvXRnWsgeAvAYFBprbEYcz7sOU04HM9OGcyjYREMs3Ih6H2oBi3GavJ2x0MG75\n'
+            'M9JSTu/yytD8GCM3s+3RncKuEAxfZIk1Gbdz0pjb+U6G43qq8/vQPKtKuAeqJHDS\n'
+            'SAER9YkKqbp0y85LbhUWNWPpHQ2zy8WB71TfYE6vBP5qjoxiqP/QGWjT/3jhCY+t\n'
+            '5k7R6XqvdvbSu1avFlEgApknzn94I+gsWQ==\n'
+            '=QuDe\n'
+            '-----END PGP MESSAGE-----'
+        )
+        self.assertEqual(c.db['host'], 'db.heck.ya')
+        self.assertEqual(c.db['pass'], encrypted_password)
 
 if __name__ == '__main__':
     unittest.main()
