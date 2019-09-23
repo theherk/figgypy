7171cbc79e5a765be02f864b9d8e84bdc45e22d5
Adam Sherwood <theherk@gmail.com>
2019-09-23 16:24:42 -0700

    Remove classifiers

--- setup.py
+++ setup.py
@@ -29,20 +29,5 @@ setup(
     platforms=['all'],
     license='MIT',
     install_requires=install_requires,
-    test_suite='tests',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: Other/Proprietary License',
-        'License :: OSI Approved :: MIT License',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Topic :: Utilities',
-    ],
+    test_suite='tests'
 )
